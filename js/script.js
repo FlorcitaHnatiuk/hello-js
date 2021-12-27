75e110c17932036aba0f2678f426210d8d368962
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2021-12-27 10:41:01 -0300

    cambios generales

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
-let nombre = prompt("Tu nombre");
-let saludo = alert("Hola " + nombre + ". Gracias por sumarte!");
+
+/* let nombre = prompt("Tu nombre");
+let apellido= prompt("Y tu apellido?");
+let temperatura = parseInt(prompt("Cuantos grados hacen?"));
+let clima = prompt("Como está el cielo?");
+
+let saludo = alert("Hola " + nombre + " " + apellido + ". Gracias por sumarte!");
+
+    if (temperatura >= 30&&clima=="soleado") {
+        alert("Que calor! andate a la pile!");
+    }
+    else if (temperatura < 30&&clima=="soleado") {
+        alert("Clima ideal");
+    }
+    else if (temperatura <= 10&&clima=="nublado") {
+        alert("Está re feo, quedate mirando pelis");
+    }
+    else if (temperatura <= 10&&clima=="soleado") {
+        alert("Abrigate y salí a pasear!");
+    }     
+    else {
+        alert("Que frio!!!");
+    } */
+    
+
+/*condicional para chequear de que año es la patente*/
+
+let patente = parseInt(prompt("ingresá tu patente"));
