e69325f16be62ef460718e744dabe21e991fa173
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2022-01-03 23:44:37 -0300

    cambios en html y js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,53 @@
-
-/*condicional para chequear de que año es la patente*/
-
-let nombre = prompt("Tu nombre");
-let apellido= prompt("Y tu apellido?");
-let patente = parseInt(prompt("ingresá tu patente"));
-
-let saludo = alert("Hola " + nombre + " " + apellido + ". Gracias por sumarte a nuestra comunidad!")
-
 const switchButton = document.getElementById('switch');
 
 switchButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark'); //toggle the HTML body the class 'dark'
-    switchButton.classList.toggle('active');//toggle the HTML button with the id='switch' with the class 'active'
+    document.body.classList.toggle('dark'); 
+    switchButton.classList.toggle('active');
 });
 
-/*var spanWidth = $('#text span').width();
-$('#text').animate( { width: spanWidth }, 3000 );
+/*condicional para chequear de que año es la patente?*/
+let espacio = " "
+let nombre = prompt("Tu nombre");
+let apellido= prompt("Y tu apellido?");
+let patente = parseInt(prompt("ingresá tu patente"));
+let contraseña = prompt("ingresá tu contraseña:");
 
-let temperatura = parseInt(prompt("Cuantos grados hacen?"));
-let clima = prompt("Como está el cielo?");
+//let saludo = alert("Hola " + nombre + " " + apellido + ". Gracias por sumarte a nuestra comunidad!")
+//me gusta mas la siguiente forma! :)
+switch (nombre) {
+    case nombre:
+        alert(`Hola ${nombre}${espacio}${apellido}. Gracias por sumarte!`);
+        break;
+}
 
-let saludo = alert("Hola " + nombre + " " + apellido + ". Gracias por sumarte!");
+//for incremental
+// for (let i = 0; i<10; i++) {
+//     alert (i);
+// }
 
-    if (temperatura >= 30&&clima=="soleado") {
-        alert("Que calor! andate a la pile!");
-    }
-    else if (temperatura < 30&&clima=="soleado") {
-        alert("Clima ideal");
-    }
-    else if (temperatura <= 10&&clima=="nublado") {
-        alert("Está re feo, quedate mirando pelis");
-    }
-    else if (temperatura <= 10&&clima=="soleado") {
-        alert("Abrigate y salí a pasear!");
-    }     
-    else {
-        alert("Que frio!!!");
-    } */
+//for decremental
+// for (let index = 10; i >= 0; i--) {
+//     alert (i);
+// }
+
+
+// let temperatura = parseInt(prompt("Cuantos grados hacen?"));
+// let clima = prompt("Como está el cielo?");
+
+// let saludo = alert("Hola " + nombre + " " + apellido + ". Gracias por sumarte!");
+
+//     if (temperatura >= 30&&clima=="soleado") {
+//         alert("Que calor! andate a la pile!");
+//     }
+//     else if (temperatura < 30&&clima=="soleado") {
+//         alert("Clima ideal");
+//     }
+//     else if (temperatura <= 10&&clima=="nublado") {
+//         alert("Está re feo, quedate mirando pelis");
+//     }
+//     else if (temperatura <= 10&&clima=="soleado") {
+//         alert("Abrigate y salí a pasear!");
+//     }     
+//     else {
+//         alert("Que frio!!!");
+//     } 
