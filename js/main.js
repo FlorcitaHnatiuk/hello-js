aec360d19a932d9488dad175593df91fcf73d4e6
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2022-01-04 11:40:58 -0300

    agregué intentos a la validacion de la password

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ switchButton.addEventListener('click', () => {
     switchButton.classList.toggle('active');
 });
 
+
 /*condicional para chequear de que año es la patente?*/
 let espacio = " "
 let nombre = prompt("Tu nombre");
@@ -17,13 +18,24 @@ let saludo = alert(`Hola ${nombre}${espacio}${apellido}. Gracias por sumarte!`);
 /*contraseña*/
 let pass = prompt("ingresá tu contraseña:");
 let wrongPass = "";
+let intentos = 1;
 while (wrongPass != pass){
     wrongPass = prompt("Ingrese nuevamente su contraseña");
+    if (intentos <= 3) {
+        alert ("contraseña correcta ingresada");
+    } else if (intentos > 3) {
+        alert ("La contraseña no ha podido ser validada. Recargá la web.")
+    } else {
+        alert ("error");
+    }
 }
+
 let welcome = alert(`Bienvenidx${espacio}${nombre}`);
 
 /*carrito de compras*/
-
+for (let item = 0; item >= 1; item++) {
+    alert (`${nombre}${espacio}, agregaste un item al carrito`);
+}
 
 
 
